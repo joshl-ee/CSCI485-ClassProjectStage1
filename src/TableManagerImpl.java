589b346208d26ccd92b8f3336c05abfb974f964d
Josh Lee <joshlee@usc.edu>
2023-03-06 19:59:45 -0800

    l

--- src/TableManagerImpl.java
+++ src/TableManagerImpl.java
@@ -140,6 +140,7 @@ public class TableManagerImpl implements TableManager{
       ArrayList<String> primaryKeys = new ArrayList<>();
 
       for (KeyValue keyvalue : keyvalues) {
+        System.out.println(keyvalue.toString());
         Tuple key = Tuple.fromBytes(keyvalue.getKey());
         System.out.println(key.getString(0));
         attributeNames.add(key.getString(0));
